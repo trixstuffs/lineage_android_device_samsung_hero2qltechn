7713f2d6567d94e0950b398702041ae05af68bcf
trixstuffs <100845834+trixstuffs@users.noreply.github.com>
2026-05-02 13:05:20 +0100

    Remove LOCAL_MODULE_TAGS from XML configurations
    Removed LOCAL_MODULE_TAGS for dsi_config.xml, netmgr_config.xml, and qmi_config.xml.

--- configs/Android.mk
+++ configs/Android.mk
@@ -4,7 +4,6 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := dsi_config.xml
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := data/dsi_config.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/data
@@ -12,7 +11,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := netmgr_config.xml
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := data/netmgr_config.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/data
@@ -20,7 +18,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := qmi_config.xml
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := data/qmi_config.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/data
