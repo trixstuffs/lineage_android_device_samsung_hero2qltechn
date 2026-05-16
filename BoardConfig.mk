b4f6a6c8f9c9e7571de3acef6c1d673d41df2aeb
trixstuffs <100845834+trixstuffs@users.noreply.github.com>
2026-05-16 19:31:47 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,16 +120,6 @@ BOARD_HARDWARE_CLASS += \
     hardware/cyanogen/cmhw
 
 # Enable dex pre-opt to speed up initial boot
-ifeq ($(HOST_OS),linux)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_PIC := true
-      ifneq ($(TARGET_BUILD_VARIANT),user)
-        # Retain classes.dex in APK's for non-user builds
-        DEX_PREOPT_DEFAULT := nostripping
-      endif
-    endif
-endif
 
 # Display
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
