ada7eef760749393963d3a53d8412da37bdc2220
trixstuffs <100845834+trixstuffs@users.noreply.github.com>
2026-05-21 12:06:04 +0100

    Update lineage_hero2qltechn.mk

--- lineage_hero2qltechn.mk
+++ lineage_hero2qltechn.mk
@@ -1,7 +1,3 @@
-# Disable dexpreopt
-WITH_DEXPREOPT := false
-DONT_DEXPREOPT_PREBUILTS := true
-
 $(call inherit-product, device/samsung/hero2qltechn/device.mk)
 
 PRODUCT_NAME := lineage_hero2qltechn
