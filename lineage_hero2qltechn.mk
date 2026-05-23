e8068fd755bf2f124e58a12a6f264b46ef329c91
trixstuffs <100845834+trixstuffs@users.noreply.github.com>
2026-05-23 13:31:37 +0100

    Update lineage_hero2qltechn.mk

--- lineage_hero2qltechn.mk
+++ lineage_hero2qltechn.mk
@@ -1,5 +1,11 @@
 $(call inherit-product, device/samsung/hero2qltechn/device.mk)
 
+# Fix dex2oat heap size
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.image-dex2oat-Xms=64m \
+    dalvik.vm.image-dex2oat-Xmx=512m \
+    dalvik.vm.dex2oat-Xms=64m \
+    dalvik.vm.dex2oat-Xmx=512m
 PRODUCT_NAME := lineage_hero2qltechn
 PRODUCT_DEVICE := hero2qltechn
 PRODUCT_MANUFACTURER := Samsung
